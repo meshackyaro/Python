c8463c71e96474e51b72d95e677f595a3eb371ae
BigBird-HQ <yaromeshack@gmail.com>
2023-11-23 07:04:04 +0100

    update

---
+++ number2_8.py
@@ -0,0 +1,14 @@
+number_zero = 0
+number_one = 1
+number_two = 2
+number_three = 3
+number_four = 4
+number_five = 5
+
+print("number\tsquare\tcube")
+print(number_zero,'\t',(number_zero*number_zero),'\t',(number_zero*number_zero*number_zero))
+print(number_one,'\t',(number_one*number_one),'\t',(number_one*number_one*number_one))
+print(number_two,'\t',(number_two*number_two),'\t',(number_two*number_two*number_two))
+print(number_three,'\t',(number_three*number_three),'\t',(number_three*number_three*number_three))
+print(number_four,'\t',(number_four*number_four),'\t',(number_four*number_four*number_four))
+print(number_five,'\t',(number_five*number_five),'\t',(number_five*number_five*number_five))
